cb5df7a148d85e71abf2713befafb3de05094b9e
Almog AmAos <33369377+Almog1339@users.noreply.github.com>
2018-12-23 17:57:50 +0200

    vcx
    vcx

---
+++ ServiceDesk1/wwwroot/assets/js/MainApp.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("app", ["ngRoute"])
+    .config(function($routeProvider,$locationProvider) {
+        $routeProvider
+            .when("/home",
+                {
+                    templateUrl: "/wwwroot/home.html"
+                })
+            .when("/Chat",
+                {
+                    templateUrl: "/wwwroot/Chat.html"
+                })
+                $locationProvider.html5Mode(true);
+    });
+
+
